d6490c571b6bea96df354b11b367369ce63ddc7b
Jenna Blumenthal <jenna.blumenthal@shopify.com>
2018-06-15 14:57:17 -0400

    cosmetic changes, create names for events

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,8 +11,7 @@ class Account < ApplicationRecord
 
   def register(params = {})
     raise ArgumentError unless params[:email].present?
-    self.is_active = true
-    self.email = params[:email]
+    account_registered(params)
   end
 
   def change_plan(new_plan_tier:)
@@ -20,13 +19,28 @@ class Account < ApplicationRecord
       raise ArgumentError, 'illegal plan transition'
     end
 
-    self.plan_tier = new_plan_tier
+    plan_changed(new_plan_tier)
   end
 
   def disable(reason:)
+    account_disabled(reason)
+  end
+
+  private
+
+  def account_registered(params)
+    self.is_active = true
+    self.email = params[:email]
+  end
+
+  def plan_changed(new_plan_tier)
+    self.plan_tier = new_plan_tier
+  end
+
+  def account_disabled(reason)
+    self.is_active = false
     if reason == DISABLED_REASONS[:manually_disabled]
       SendSorryToSeeYouGoEmailJob.perform_later(id)
     end
-    self.is_active = false
   end
 end
